9756f0f7e634df2bf11af87c6e281c4270cb1b8b
rae charles <r_charles87@yahoo.com>
2024-02-16 08:58:29 -0500

    brought in data from yelp api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
+import { useEffect, useState } from 'react';
 import './App.css';
-import Home from './Pages/Home/Home';
+import axios from 'axios';
+
+const API_URL = process.env.REACT_APP_API_URL;
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 function App() {
+  const [restaurants, setRestaurants] = useState([])
+ 
+  
+useEffect(()=>{
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(`${'https://corsproxy.io/?'}${API_URL}`, {
+        headers: {
+          'Authorization': `Bearer ${API_KEY}`,
+        },
+        params: {
+          "term": "restaurant",
+          "location":"10475"
 
+        }
+      });
+      setRestaurants(response.data.businesses)
 
+      // console.log(response.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  fetchData()
+},[])
+
+console.log(restaurants)
 
   return (
     <div className="App">
-     hello world
+     hello world   
     </div>
   );
 }
