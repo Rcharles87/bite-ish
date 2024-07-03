f99f71d8369579887fa1bc84b655c3b9a91f547d
rae charles <r_charles87@yahoo.com>
2024-07-03 07:37:17 -0400

    added simple nav bar to create login and signup pages

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,17 +1,28 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./NavBar.css";
 
 function Navbar() {
   return (
-    <div className='navBar'>
-      <Link to="/">
-      <button>Home</button>
-      </Link>
-      <Link to="/about">
-      <button>About Us</button>
-      </Link>
+    <div className="navBar_container">
+      <div className="left_container">
+        <div className="burger_container">
+        Menu
+        </div>
+        <div className="logo_container">
+          <Link to="/">Logo</Link>
+        </div>
+      </div>
+      <div className="account_btns">
+        <Link to="/Login">
+          <button>Login</button>
+        </Link>
+        <Link to="/Signup">
+          <button>Sign up</button>
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
