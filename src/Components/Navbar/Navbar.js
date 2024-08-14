60770b42ee0e87cfecc3ce2a1d7ff3f41ceb1eab
rae charles <r_charles87@yahoo.com>
2024-08-14 11:11:19 -0400

    changed icons and styled buttons, updated css to refelect wire frame

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -2,23 +2,25 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./NavBar.css";
 
+import MenuIcon from '@mui/icons-material/Menu';
+
 function Navbar() {
   return (
     <div className="navBar_container">
       <div className="left_container">
         <div className="burger_container">
-        Menu
+        <MenuIcon />
         </div>
         <div className="logo_container">
-          <Link to="/">Logo</Link>
+          <Link to="/">Bite-ish</Link>
         </div>
       </div>
       <div className="account_btns">
         <Link to="/Login">
-          <button>Login</button>
+          Log in
         </Link>
         <Link to="/Signup">
-          <button>Sign up</button>
+          Sign up
         </Link>
       </div>
     </div>
