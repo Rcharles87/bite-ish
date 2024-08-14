2bfff0cc0370ba94ce1b3cfc0ad30bf23e7d3748
rae charles <r_charles87@yahoo.com>
2024-08-14 09:16:56 -0400

    added dummy data to mock backend, added mui icons for navbar styling, added photos for landing page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React from "react";
+import "./App.css"
 import { Route, Routes } from "react-router-dom";
 import Error from "./Pages/Error/Error.js"
 import Home from "./Pages/Home/Home.js"
 import About from "./Pages/About/About.js"
 import Navbar from "./Components/Navbar/Navbar.js";
+import data from "./data.js"
 // import RestaurantList from "./Components/RestaurantList/RestaurantList.js";
 
 
 
 function App() {
 
-
+console.log(data[0])
   
   
   return(
-    <div>
+    <div className="App">
       <Navbar/>
       <Routes>
         <Route path="*" element={<Error/>}/>
